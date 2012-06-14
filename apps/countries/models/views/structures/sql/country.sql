af857c1fb7b1b8aadceb41d5110026f2282a2e32
Willem van Heemstra <wvanheemstra@xs4all.nl>
2012-06-14 17:29:44 +0100

    Added the views to the tables

---
+++ apps/countries/models/views/structures/sql/country.sql
@@ -0,0 +1,23 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : wvanheem_core_local
+ Source Server Version : 50509
+ Source Host           : 127.0.0.1
+ Source Database       : core
+
+ Target Server Version : 50509
+ File Encoding         : utf-8
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  View structure for `country`
+-- ----------------------------
+DROP VIEW IF EXISTS `country`;
+CREATE VIEW `person` AS
+  SELECT * FROM `tbl_country`;
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ apps/skills/models/views/structures/sql/skill.sql
@@ -0,0 +1,23 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : wvanheem_core_local
+ Source Server Version : 50509
+ Source Host           : 127.0.0.1
+ Source Database       : core
+
+ Target Server Version : 50509
+ File Encoding         : utf-8
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  View structure for `skill`
+-- ----------------------------
+DROP VIEW IF EXISTS `skill`;
+CREATE VIEW `person` AS
+  SELECT * FROM `tbl_skill`;
+
+SET FOREIGN_KEY_CHECKS = 1;

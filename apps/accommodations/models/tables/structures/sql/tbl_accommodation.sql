f68409e55e2f9c6da33d94ff3db96b0b09b0524c
Willem van Heemstra <wvanheemstra@xs4all.nl>
2012-06-14 20:54:29 +0100

    Updated with field types, keys, and indexes

--- apps/accommodations/models/tables/structures/sql/tbl_accommodation.sql
+++ apps/accommodations/models/tables/structures/sql/tbl_accommodation.sql
@@ -9,7 +9,7 @@
  Target Server Version : 50509
  File Encoding         : utf-8
 
- Date: 06/14/2012 14:35:46 PM
+ Date: 06/14/2012 20:50:58 PM
 */
 
 SET NAMES utf8;
@@ -20,11 +20,14 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_accommodation`;
 CREATE TABLE `tbl_accommodation` (
-  `__kp_AccommodationID` float NOT NULL,
-  `AccommodationName` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `_kf_WhereaboutsID` float DEFAULT NULL,
-  `_kf_KindOfAccommodationID` float DEFAULT NULL,
-  PRIMARY KEY (`__kp_AccommodationID`)
+  `__kp_AccommodationID` int(11) NOT NULL AUTO_INCREMENT,
+  `AccommodationName` varchar(255) COLLATE utf8_bin NOT NULL,
+  `_kf_WhereaboutsID` int(11) NOT NULL,
+  `_kf_KindOfAccommodationID` int(11) NOT NULL,
+  PRIMARY KEY (`__kp_AccommodationID`),
+  UNIQUE KEY `__kp_AccommodationID` (`__kp_AccommodationID`),
+  KEY `_kf_WhereaboutsID` (`_kf_WhereaboutsID`),
+  KEY `_kf_KindOfAccommodationID` (`_kf_KindOfAccommodationID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 SET FOREIGN_KEY_CHECKS = 1;

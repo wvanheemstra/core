334f05c4562b0564a011232fcb1387ae2e8f052d
Willem van Heemstra <wvanheemstra@xs4all.nl>
2012-06-14 14:13:56 +0100

    Added countries for structure and with data

---
+++ public_html/apps/countries/models/tables/structures/sql/tbl_country.sql
@@ -0,0 +1,29 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : wvanheem_core_local
+ Source Server Version : 50509
+ Source Host           : 127.0.0.1
+ Source Database       : core
+
+ Target Server Version : 50509
+ File Encoding         : utf-8
+
+ Date: 06/14/2012 14:07:37 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `tbl_country`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_country`;
+CREATE TABLE `tbl_country` (
+  `__kp_CountryID` float NOT NULL,
+  `CountryCode` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `CountryName` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`__kp_CountryID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+SET FOREIGN_KEY_CHECKS = 1;

4fac8c21ead32db9008c0c0494c9743eb880ed29
Willem van Heemstra <wvanheemstra@xs4all.nl>
2012-07-09 13:57:04 +0100

    Updated

--- public_html/core/components/core/apps/core/app2_embed.js
+++ public_html/core/components/core/apps/core/app2_embed.js
@@ -1,1 +1,43 @@
 Ext.Loader.setConfig({ enabled: true });
+Ext.application({
+	name: 'core',
+	appFolder: 'core/components/core/apps/core/app',
+	autoCreateViewport: true,
+	controllers: [
+		'Parties',
+		'Organisations',
+		'Persons'
+	],
+	launch: function() {
+		// This is fired as soon as the page is ready
+		var panel = new Ext.Panel({
+                        renderTo: 'content',
+                        layout: 'fit',
+                        items: [
+                                {
+                                        title: 'Some Title',
+                                        html : 'Some Text'
+								//		xtype: 'partylist',
+								//		xtype: 'organisationlist'
+								//		xtype: 'personlist'
+                                }
+                        ]
+        });
+		(function() {
+                    Ext.Loader.setConfig({
+                        enabled : true,
+                        paths   : {
+                            core : 'core/components/core/apps/core'
+                        } 
+                    });
+                 
+                //    Ext.require('core.app.view.UserEditorWindow');
+                 
+                    Ext.onReady(function() {
+                //        Ext.create('core.app.view.UserEditorWindow').show();
+                    });
+        })();
+		//pass along browser window resize events to the panel
+        Ext.EventManager.onWindowResize(panel.doLayout, panel);
+	}
+});

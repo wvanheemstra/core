14aeae10df031d57ec0a18b2e27926b77fc57940
Willem van Heemstra <wvanheemstra@xs4all.nl>
2012-07-09 22:43:14 +0100

    Updated

--- public_html/core/components/core/apps/core/app/view/Viewport.js
+++ public_html/core/components/core/apps/core/app/view/Viewport.js
@@ -1,7 +1,5 @@
 Ext.define('core.view.Viewport', {
     extend: 'Ext.container.Viewport',
-	layout: 'fit',
-	renderTo: 'content',
 	requires: [
 		'core.view.NewStation',
         'core.view.SongControls',
@@ -13,62 +11,75 @@ Ext.define('core.view.Viewport', {
 	// we can easily adjust the application’s overall layout in one single place (this Viewport), 
 	// adding to the maintainability and flexibility of our architecture.
 	initComponent: function() {
-		this.items = {
-            dockedItems: [{
-                dock: 'top',
-                xtype: 'toolbar',
-                height: 80,
-                items: [{
-                    xtype: 'newstation',
-                    width: 150
-                }, {
-                    xtype: 'songcontrols',
-                    height: 70,
-                    flex: 1
-                }, {
-                    xtype: 'component',
-                    html: 'Core<br>Internet Radio'
-                }]
-            }],
-            layout: {
-                type: 'hbox',
-                align: 'stretch'
-            },
+		var panel = new Ext.Panel({
+            renderTo: 'content',
+            layout: 'fit',
+			dockedItems: [{
+				dock: 'top',
+				xtype: 'toolbar',
+				height: 80,
+				items: [{
+					xtype: 'newstation',
+					width:150
+				}, {
+					xtype: 'songcontrols',
+					flex: 1
+				}, {
+					xtype: 'component',
+					hmtl: 'Core<br>Internet Radio'
+				}]
+			}],
+			layout: {
+				type: 'hbox',
+				align: 'stretch'
+			},
             items: [{
-                width: 250,
-                xtype: 'panel',
-				id: 'west-region',
-                layout: {
-                    type: 'vbox',
-                    align: 'stretch'
-                },
-                items: [{
-                    xtype: 'stationslist',
-                    flex: 1
-                }, {
-                    html: 'Ad',
-                    height: 250,
-                    xtype: 'panel'
-                }]
-            }, {
-                xtype: 'container',
-                flex: 1,
-                layout: {
-                    type: 'vbox',
-                    align: 'stretch'
-                },
-                items: [{
-                    xtype: 'recentlyplayedscroller',
-                    height: 250
-                }, {
-                    xtype: 'songinfo',
-                    flex: 1
-                }]
+                title: 'Some Title',
+                layout : {
+					type: 'hbox',
+					align: 'stretch'
+				},
+				items: [{
+					width: 250,
+					xtype: 'panel',
+					layout: {
+						type: 'vbox',
+						align: 'stretch'
+					},
+					items: [{
+						xtype: 'stationslist',
+						flex: 1
+					}, {
+						html: 'Ad',
+						height: 250,
+						xtype: 'panel'
+					}]
+				}, {
+					xtype: 'container',
+					flex: 1,
+					layout: {
+						type: 'vbox',
+						align: 'stretch'
+					},
+					items: [{
+						xtype: 'recentlyplayedscroller',
+						height: 250
+					}, {
+						xtype: 'songinfo',
+						flex: 1
+					}]
+				}]
             }]
-        };
+        });
 		
 		//pass along browser window resize events to the panel
-    //    Ext.EventManager.onWindowResize(panel.doLayout, panel);
+        Ext.EventManager.onWindowResize(panel.doLayout, panel);
+		
+		//Ext.EventManager.onWindowResize(function() {
+			// pass "true" to get the contendWidth (excluding border/padding/etc.)
+		//	mainPanel.setWidth(Ext.getBody().getWidth(true));
+			// seems to be no need to call mainPanel.doLayout() here in my situation
+		//});
 		
 		this.callParent();
     }

17e8f80d0b94d68ff5fa58c83a25613571e67038
Willem van Heemstra <wvanheemstra@xs4all.nl>
2012-08-23 12:23:24 +0100

    Updated

--- public_html/core/components/core/apps/core/app/store/Persons.js
+++ public_html/core/components/core/apps/core/app/store/Persons.js
@@ -1,18 +1,75 @@
+/**
+ * core.store.Persons
+ * @extends Ext.data.Store
+ */
 Ext.define('core.store.Persons', {
-    extend: 'Ext.data.Store',
-	model: 'core.model.Person',
-	autoLoad: true,
+	extend: 'Ext.data.Store',
+	constructor: function(config) {
+		config = config || {};
+		config.autoDestroy = true;
+		config.requires = 'core.model.Person';
+		config.model = 'core.model.Person';
+		config.proxy = {
+			type: 'ajax',
+			actionMethods: 'POST', // wvh: Do we need this to be defined??
+			api: {
+				create: (local ? urlCreate.local : urlCreate.remote),				
+				read: (local ? urlRead.local : urlRead.remote),
+				update: (local ? urlUpdate.local : urlUpdate.remote),					
+				destroy: (local ? urlDestroy.local : urlDestroy.remote)
+			},
+			reader: {
+				type: 'json',
+				root: 'data', // change to 'result' when used with MQL
+				totalProperty: 'total',
+				successProperty: 'success',
+				messageProperty: 'message'
+			},
+			writer: {
+				type: 'json',
+				writeAllFields: true,
+				allowSingle: false,
+				root: 'data'
+			},
+			listeners: {
+			//	exception: function(proxy, response, operation){
+			//		Ext.MessageBox.show({
+			//			title: 'REMOTE EXCEPTION',
+			//			msg: operation.getError(),
+			//			icon: Ext.MessageBox.ERROR,
+			//			buttons: Ext.Msg.OK
+			//		});
+			//	}				
+			}
+		};
+		config.remoteSort = false;
+		config.sorters = [{
+			property: 'PersonLastName',
+			direction: 'ASC'
+		}];
+		config.pageSize = 10; // was 50
+		// call the superclass's constructor
+		core.store.Persons.superclass.constructor.call(this, config);			
+	}
+});	
 
-    proxy: {
-        type: 'ajax',
-        api: {
-			read: 'http://localhost/core/components/core/apps/core/data/persons.json',
-			update: 'http://localhost/core/components/core/apps/core/data/updatePersons.json'
-		},
-        reader: {
-            type: 'json',
-            root: 'persons',
-            successProperty: 'success'
-        }
-    }
-});
+// configure whether filtering is performed locally or remotely (initially)
+var local = true;
+
+// configure CRUD urls
+var urlCreate = {
+	local: 'http://localhost/core/components/core/apps/core/data/create_person.php',
+	remote: 'http://localhost/core/components/core/apps/core/data/create_person.php'
+};
+var urlRead = {
+	local: 'http://localhost/core/components/core/apps/core/data/read_person.php',
+	remote: 'http://localhost/core/components/core/apps/core/data/read_person.php'
+};	
+var urlUpdate = {
+	local: 'http://localhost/core/components/core/apps/core/data/update_person.php',
+	remote: 'http://localhost/core/components/core/apps/core/data/update_person.php'
+};
+var urlDestroy = {
+	local: 'http://localhost/core/components/core/apps/core/data/destroy_person.php',
+	remote: 'http://localhost/core/components/core/apps/core/data/destroy_person.php'
+};
--- public_html/core/components/core/apps/core/mql-multiple-persons-crud-filter-live-feed.js
+++ public_html/core/components/core/apps/core/mql-multiple-persons-crud-filter-live-feed.js
@@ -11,6 +11,7 @@ Ext.require([
     'Ext.ux.ajax.JsonSimlet',
     'Ext.ux.ajax.SimManager',
 	'core.model.Person',
+	'core.store.Persons',
 	'core.store.Salutations',
 	'core.store.Genders',
 	'core.store.Nationalities'
@@ -21,27 +22,6 @@ Ext.Loader.onReady(function() {
 	Ext.QuickTips.init();
 
 	// **************************************** START OF VARIABLES ************************************************ //
-
-    // configure whether filtering is performed locally or remotely (initially)
-    var local = true;
-	
-    // configure CRUD urls
-    var urlCreate = {
-		local: 'http://localhost/core/components/core/apps/core/data/create_person.php',
-        remote: 'http://localhost/core/components/core/apps/core/data/create_person.php'
-    };
-    var urlRead = {
-		local: 'http://localhost/core/components/core/apps/core/data/read_person.php',
-        remote: 'http://localhost/core/components/core/apps/core/data/read_person.php'
-    };	
-    var urlUpdate = {
-		local: 'http://localhost/core/components/core/apps/core/data/update_person.php',
-        remote: 'http://localhost/core/components/core/apps/core/data/update_person.php'
-    };
-    var urlDestroy = {
-		local: 'http://localhost/core/components/core/apps/core/data/destroy_person.php',
-        remote: 'http://localhost/core/components/core/apps/core/data/destroy_person.php'
-    };
 	
     var filters = {
         ftype: 'filters',
@@ -70,61 +50,6 @@ Ext.Loader.onReady(function() {
 	// **************************************** END OF MODELS ***************************************************** //	
 
 	// **************************************** START OF STORES ***************************************************** //
-
-	/**
-     * core.store.Persons
-     * @extends Ext.data.Store
-	 */
-	Ext.define('core.store.Persons', {
-        extend: 'Ext.data.Store',
-		constructor: function(config) {
-			config = config || {};
-			config.autoDestroy = true;
-			config.requires = 'core.model.Person';
-            config.model = 'core.model.Person';
-			config.proxy = {
-				type: 'ajax',
-				actionMethods: 'POST', // wvh: Do we need this to be defined??
-				api: {
-					create: (local ? urlCreate.local : urlCreate.remote),				
-					read: (local ? urlRead.local : urlRead.remote),
-					update: (local ? urlUpdate.local : urlUpdate.remote),					
-					destroy: (local ? urlDestroy.local : urlDestroy.remote)
-				},
-				reader: {
-					type: 'json',
-					root: 'data', // change to 'result' when used with MQL
-					totalProperty: 'total',
-					successProperty: 'success',
-					messageProperty: 'message'
-				},
-				writer: {
-					type: 'json',
-					writeAllFields: true,
-					allowSingle: false,
-					root: 'data'
-				},
-				listeners: {
-				//	exception: function(proxy, response, operation){
-				//		Ext.MessageBox.show({
-				//			title: 'REMOTE EXCEPTION',
-				//			msg: operation.getError(),
-				//			icon: Ext.MessageBox.ERROR,
-				//			buttons: Ext.Msg.OK
-				//		});
-				//	}				
-				}
-			};
-			config.remoteSort = false;
-			config.sorters = [{
-				property: 'PersonLastName',
-				direction: 'ASC'
-			}];
-			config.pageSize = 10; // was 50
-            // call the superclass's constructor
-            core.store.Persons.superclass.constructor.call(this, config);			
-		}
-	});		
 
 	var storeSalutations = new core.store.Salutations();	
 	function get_SalutationAbbreviation(value){

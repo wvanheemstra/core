3282b47b2bd067b356313e1eb73bd2817c7d52f9
Willem van Heemstra <wvanheemstra@xs4all.nl>
2012-08-26 10:46:29 +0100

    Updated

---
+++ public_html/core/components/core/apps/core/data/read_nationality.php
@@ -0,0 +1,35 @@
+<?php   
+
+//database parameters
+$host='localhost';
+$port='3306';
+$user='root'; 
+$pw='';
+$db='core';
+$table='nationality';
+   
+//make database connection
+$connection = mysql_connect("localhost", $user, $pw ) or
+   die("Could not connect: " . mysql_error());
+mysql_set_charset("UTF8", $connection);   
+mysql_select_db($db) or die("Could not select database");
+
+listNationalities();
+    
+function listNationalities() 
+{
+    $sql = 'SELECT * FROM nationality';
+    
+    $result = mysql_query($sql); // result set
+    
+    while($rec = mysql_fetch_array($result, MYSQL_ASSOC)){
+        $arr[] = $rec;
+    };
+
+    $rows = 10; // dummy value; used for paging; not needed here
+    $data = json_encode($arr);  //encode the data in json format
+      
+    echo '({"total":"' . $rows . '","data":' . $data . '})';
+}
+
+?>

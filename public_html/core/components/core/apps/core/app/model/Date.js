d9444209e5c05de0a4de0adb15a92d6f8f63ca43
wvanheemstra <wvanheemstra@xs4all.nl>
2012-09-16 14:47:53 +0100

    Updated

--- public_html/core/components/core/apps/core/app/model/Date.js
+++ public_html/core/components/core/apps/core/app/model/Date.js
@@ -6,8 +6,8 @@ Ext.define('core.model.Date', {
 	extend: 'Ext.data.Model',
 	fields: [
 		{ name: 'kp_DateID', type: 'int', defaultValue: '0'},
-		{ name: 'DateStart', type: 'date'}, 
-		{ name: 'DateFinish', type: 'date'}
+		{ name: 'DateStart', type: 'string'}, // Use string instead of date !!
+		{ name: 'DateFinish', type: 'string'} // Use string instead of date !!
 	],
 	idProperty: 'kp_DateID',
 	requires: [

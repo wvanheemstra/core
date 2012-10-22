183b17d69dc3262604b8f145e6aa2405c599c7ff
Willem van Heemstra <wvanheemstra@xs4all.nl>
2012-10-22 10:37:31 +0100

    Updated

--- public_html/core/components/core/apps/core/app/model/PersonModel.js
+++ public_html/core/components/core/apps/core/app/model/PersonModel.js
@@ -20,10 +20,10 @@ Ext.define('core.model.PersonModel', {
 		'core.model.GenderModel','core.model.SalutationModel','core.model.NationalityModel','core.model.DateModel','core.model.PersonGroupModel'
 	],
 	associations: [
-		{ name: 'gender', type: 'hasOne', model: 'core.model.GenderModel', primaryKey: 'kp_GenderID', foreignKey: 'kf_GenderID' },
-		{ name: 'salutation', type: 'hasOne', model: 'core.model.SalutationModel', primaryKey: 'kp_SalutationID', foreignKey: 'kf_SalutationID' },
-		{ name: 'nationality', type: 'hasOne', model: 'core.model.NationalityModel', primaryKey: 'kp_NationalityID', foreignKey: 'kf_NationalityID' },
-		{ name: 'date', type: 'hasOne', model: 'core.model.DateModel', primaryKey: 'kp_DateID', foreignKey: 'kf_DateID' },
-		{ name: 'personsgroups', type: 'hasMany', model: 'core.model.PersonGroupModel', primaryKey: 'kf_PersonID', foreignKey: 'kp_PersonID' } // NOTE: the primary key in PersonGroup is kf_PersonID (!), the foreign key in Person is kp_PersonID (!)
+		{ name: 'gender', type: 'hasOne', model: 'core.model.GenderModel', primaryKey: 'kp_GenderID', foreignKey: 'kf_GenderID', getterName: 'getGenderModel', setterName: 'setGenderModel' },
+		{ name: 'salutation', type: 'hasOne', model: 'core.model.SalutationModel', primaryKey: 'kp_SalutationID', foreignKey: 'kf_SalutationID', getterName: 'getSalutationModel', setterName: 'setSalutationModel' },
+		{ name: 'nationality', type: 'hasOne', model: 'core.model.NationalityModel', primaryKey: 'kp_NationalityID', foreignKey: 'kf_NationalityID', getterName: 'getNationalityModel', setterName: 'setNationalityModel' },
+		{ name: 'date', type: 'hasOne', model: 'core.model.DateModel', primaryKey: 'kp_DateID', foreignKey: 'kf_DateID', getterName: 'getDateModel', setterName: 'setDateModel' },
+		{ name: 'personsgroups', type: 'hasMany', model: 'core.model.PersonGroupModel', primaryKey: 'kf_PersonID', foreignKey: 'kp_PersonID', getterName: 'getPersonGroupModel', setterName: 'setPersonGroupModel' } // NOTE: the primary key in PersonGroup is kf_PersonID (!), the foreign key in Person is kp_PersonID (!)
 	]
 });

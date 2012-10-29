8c97049634e115165796b48a1e53678ec8c75ae1
Willem van Heemstra <wvanheemstra@xs4all.nl>
2012-10-29 17:07:57 +0000

    Updated

--- public_html/core/components/core/apps/core/app/model/OrganisationModel.js
+++ public_html/core/components/core/apps/core/app/model/OrganisationModel.js
@@ -1,11 +1,13 @@
-/**
- * core.model.OrganisationModel
- * @extends Ext.data.Model
- */
 Ext.define('core.model.OrganisationModel', {
     extend: 'Ext.data.Model',
 	fields: [
 		{name: 'kp_OrganisationID',  type: 'int'}
     ],
-    idProperty:'kp_OrganisationID'
+    idProperty:'kp_OrganisationID',
+	requires: [
+		'core.model.MembershipModel'
+	],
+	associations: [
+	{ name: 'memberships', type: 'hasMany', model: 'core.model.MembershipModel', primaryKey: 'kf_OrganisationID', foreignKey: 'kp_OrganisationID', associationKey: 'memberships', getterName: 'getMembershipModel', setterName: 'setMembershipModel', autoLoad: true } // NOTE: the primary key in Membership is kf_OrganisationID (!), the foreign key in Organsiation is kp_OrganisationID (!)
+	]
 });

e67a109242a1a1e01ef32a6412d1aac2f978fb94
wvanheemstra <wvanheemstra@icloud.com>
2013-09-15 12:36:53 +0100

    Updated

---
+++ public/app/event/person/Event.js
@@ -0,0 +1,155 @@
+/**
+ * The person event contains data and event types to perform CRUD operations on persons.
+ */
+Ext.define("Core.event.person.Event", {
+    extend: "FlowMVC.mvc.event.AbstractEvent",
+    
+    statics: {
+
+        /**
+         * The get person slide event type.
+         */
+        GET_PERSON_SLIDE:          "getPersonSlide",
+    	
+        /**
+         * The get person slide success event type.
+         */
+        GET_PERSON_SLIDE_SUCCESS:  "getPersonSlideSuccess",
+
+        /**
+         * The get person slide failure event type.
+         */
+        GET_PERSON_SLIDE_FAILURE:  "getPersonSlideFailure",	
+	
+        /**
+         * The get person list event type.
+         */
+        GET_PERSON_LIST:          "getPersonList",
+    	
+        /**
+         * The get person list success event type.
+         */
+        GET_PERSON_LIST_SUCCESS:  "getPersonListSuccess",
+
+        /**
+         * The get person list failure event type.
+         */
+        GET_PERSON_LIST_FAILURE:  "getPersonListFailure",
+        
+        /**
+         * The get person tile event type.
+         */
+        GET_PERSON_TILE:          "getPersonTile",
+    	
+        /**
+         * The get person tile success event type.
+         */
+        GET_PERSON_TILE_SUCCESS:  "getPersonTileSuccess",
+
+        /**
+         * The get person tile failure event type.
+         */
+        GET_PERSON_TILE_FAILURE:  "getPersonTileFailure",        
+
+        /**
+         * The get person modal event type.
+         */
+        GET_PERSON_MODAL:          "getPersonModal",
+    	
+        /**
+         * The get person modal success event type.
+         */
+        GET_PERSON_MODAL_SUCCESS:  "getPersonModalSuccess",
+
+        /**
+         * The get person modal failure event type.
+         */
+        GET_PERSON_MODAL_FAILURE:  "getPersonModalFailure",	
+		
+        /**
+         * The get person event type.
+         */
+        GET_PERSON:               "getPerson",
+
+        /**
+         * The get person success event type.
+         */
+        GET_PERSON_SUCCESS:       "getPersonSuccess",
+
+        /**
+         * The get person failure event type.
+         */
+        GET_PERSON_FAILURE:       "getPersonFailure",
+
+        /**
+         * The create person event type.
+         */
+        CREATE_PERSON:            "createPerson",
+
+        /**
+         * The create person success event type.
+         */
+        CREATE_PERSON_SUCCESS:    "createPersonSuccess",
+
+        /**
+         * The create person failure event type.
+         */
+        CREATE_PERSON_FAILURE:    "createPersonFailure",
+
+        /**
+         * The update person event type.
+         */
+        UPDATE_PERSON:            "updatePerson",
+
+        /**
+         * The update person success event type.
+         */
+        UPDATE_PERSON_SUCCESS:    "updatePersonSuccess",
+
+        /**
+         * The update person failure event type.
+         */
+        UPDATE_PERSON_FAILURE:    "updatePersonFailure",
+
+        /**
+         * The delete person event type.
+         */
+        DELETE_PERSON:            "deletePerson",
+
+        /**
+         * The delete person success event type.
+         */
+        DELETE_PERSON_SUCCESS:    "deletePersonSuccess",
+
+        /**
+         * The delete person failure event type.
+         */
+        DELETE_PERSON_FAILURE:    "deletePersonFailure"
+    },
+
+    /**
+     * @property {Number} id [readOnly=false]
+     * The id of a person.
+     */
+    id: null,
+
+    /**
+     * @property {Core.model.person.Model} person [readOnly=false]
+     * A person to perform CRUD actions on.
+     */
+    person: null,
+
+    /**
+     * Constructor. Allows the username and password for authentication to be set on the event.
+     *
+     * @param {String} type The event type.
+     * @param {Number} id The id of the person the CRUD operation is acting on.
+     * @param {Core.model.person.Model} person The person the CRUD operation is acting on.     
+     */
+    constructor: function(type, id, person) {
+        this.callParent(arguments);
+
+        this.id = id;
+        this.person = person;
+    }
+})	

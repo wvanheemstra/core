d4459c3dbe0d7956de11966a26bc00b7f4fff844
wvanheemstra <wvanheemstra@icloud.com>
2013-09-15 13:11:19 +0100

    Updated

---
+++ public/app/model/person/Model.js
@@ -0,0 +1,28 @@
+/**
+ * The person object representing a person.
+ */
+Ext.define("Core.model.person.Model", {
+    extend: "Ext.data.Model",
+    // Touch uses properties inside of config
+	config: {
+	    idProperty: "id",
+	    fields: [
+	        { name: "id",      type: "int"     },
+	        { name: "name",    type: "string"  }
+	    ],
+	    validations: [
+	        { type: "presence", field: "id" },
+	        { type: "presence", field: "name",     message: "Please enter a name." }
+	    ]
+	},//eof config
+	// Ext requires properties outside of config
+    idProperty: "id",
+	fields: [
+        { name: "id",      type: "int"     },
+        { name: "name",    type: "string"  }
+	],
+    validations: [
+        { type: "presence", field: "id" },
+        { type: "presence", field: "name",     message: "Please enter a name." }
+    ]	
+});

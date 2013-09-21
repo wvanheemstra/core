5ab200d5ca6a0e7e4807a1bfb98dee77a0b53581
wvanheemstra <wvanheemstra@icloud.com>
2013-09-21 16:00:59 +0100

    Updated

--- public/app/store/person/Store.js
+++ public/app/store/person/Store.js
@@ -6,28 +6,28 @@ Ext.define("Core.store.person.Store", {
     // Touch uses properties inside of config
 	config: {
 	    model: "Core.model.person.Model",
-	    sorters: "name",
+	    sorters: "personLastName",
 	    isAutoUpdate: true,
 	    grouper: {
 	        groupFn: function(record) {
 	            try {
-	                return record.get("name")[0];
+	                return record.get("personLastName")[0].toUpperCase(); // so 'van Halen' comes under 'V'   
 	            } catch(err) {
-	
+					console.log(err);
 	            }
 	        }
 	    }
 	},//eof config
 	// Ext requires properties outside of config
     model: "Core.model.person.Model",
-    sorters: "name",
+    sorters: "personLastName",
     isAutoUpdate: true,
     grouper: {
         groupFn: function(record) {
             try {
-                return record.get("name")[0];
+                return record.get("personLastName")[0];
             } catch(err) {
-
+				console.log(err);
             }
         }
     }	

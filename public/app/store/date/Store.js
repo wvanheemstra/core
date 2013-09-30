e9749db7bc20fe971fed18439b8eb8538828b0d9
wvanheemstra <wvanheemstra@icloud.com>
2013-09-30 08:27:33 +0100

    Updated

---
+++ public/app/store/date/Store.js
@@ -0,0 +1,36 @@
+/**
+ * Contains the list of date objects.
+ */
+Ext.define("Core.store.date.Store", {
+    extend: "Core.store.date.base.Store",
+    // Touch uses properties inside of config
+	config: {
+	    storeId: "dateStore", // Required for automatic registration
+	    model: "Core.model.date.Model",
+	    sorters: "DateStart",
+	    isAutoUpdate: true,
+	    grouper: {
+	        groupFn: function(record) {
+	            try {
+	                return record.get("DateStart")[0].toUpperCase(); // so 'van Halen' comes under 'V'   
+	            } catch(err) {
+					console.log(err);
+	            }
+	        }
+	    }
+	},//eof config
+	// Ext requires properties outside of config
+	storeId: "dateStore", // Required for automatic registration
+    model: "Core.model.date.Model",
+    sorters: "DateStart",
+    isAutoUpdate: true,
+    grouper: {
+        groupFn: function(record) {
+            try {
+                return record.get("DateStart")[0].toUpperCase(); // so 'van Halen' comes under 'V'
+            } catch(err) {
+				console.log(err);
+            }
+        }
+    }	
+});

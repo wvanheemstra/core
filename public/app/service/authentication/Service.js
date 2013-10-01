84fe95fc994884ef89eceef0fb2a83e4ae07adca
wvanheemstra <wvanheemstra@icloud.com>
2013-10-01 19:47:03 +0100

    Updated

--- public/app/service/authentication/Service.js
+++ public/app/service/authentication/Service.js
@@ -17,30 +17,34 @@ Ext.define("Core.service.authentication.Service", {
     authenticate: function(username, password) {
         this.logger.debug("authenticate: username = " + username + ", password = " + password);
 
-        var deferred = Ext.create("Deft.promise.Deferred");
+        //ORIGINAL var deferred = Ext.create("Deft.promise.Deferred");
+		var token = this.getTokenOrPromise(); // NEW
         var me = this;
 
         Ext.Ajax.request({
             url: "data/login-success.json",
 //            url: "http://localhost:8080/SenchaDemo",
-            method: "post",
+            method: "get", // For local files use 'get', for remote use 'post'
             params: {
-                j_username: username,
-                j_password: password
+                username: username,
+                password: password
             },
 
             success: function(response) {
-                this.logger.debug("authenticate.success");
-                me.success(response, deferred);
+                console.log("authenticate.success"); // use console, as logger is undefined
+                //ORIGINAL me.success(response, deferred);
+				me.success(response, token); // NEW
             },
 
             failure: function(response) {
-                this.logger.debug("authenticate.failure");
-                me.failure(response, deferred);
+                console.log("authenticate.failure"); // use console, as logger is undefined
+                //ORIGINAL me.failure(response, deferred);
+				me.failure(response, token); // NEW
             }
         });
 
-        return deferred.promise;
+        //ORIGINAL return deferred.promise;
+		return token; // NEW
     },
 
     /**
@@ -49,25 +53,28 @@ Ext.define("Core.service.authentication.Service", {
     logout: function() {
         this.logger.debug("logout");
 
+		var token = this.getTokenOrPromise(); // NEW
         var me = this;
 
         Ext.Ajax.request({
             url: "data/logout-success.json",
-            method: "post",
+            method: "get", // For local files use 'get', for remote use 'post'
 
             success: function(response) {
-                this.logger.debug("logout.success");
+                console.log("logout.success"); // use console, as logger is undefined
 
                 var response = Ext.JSON.decode(response.responseText);
-                me.success(response);
+                //ORIGINAL me.success(response);
+				me.success(response, token); // NEW
             },
 
             failure: function(response) {
-                this.logger.debug("logout.failure");
-                me.failure(response);
+                console.log("logout.failure"); // use console, as logger is undefined
+                //ORIGINAL me.failure(response);
+				me.failure(response, token); // NEW
             }
         });
+		return token; // NEW
     }
-
 });
 
--- public/app/service/person/Service.js
+++ public/app/service/person/Service.js
@@ -14,7 +14,8 @@ Ext.define("Core.service.person.Service", {
     getPersonList: function() {
         this.logger.debug("getPersonList");
 
-        var deferred = Ext.create("Deft.promise.Deferred");
+        //ORIGINAL var deferred = Ext.create("Deft.promise.Deferred");
+		var token = this.getTokenOrPromise(); // NEW
         var me = this;
 		
         Ext.Ajax.request({
@@ -29,78 +30,20 @@ Ext.define("Core.service.person.Service", {
             success: function(response) {
                 //this.logger.debug("getPersonList.success"); // logger is undefined here
 				console.log("getPersonList.success");
-                me.success(response, deferred);
+                //ORIGINAL me.success(response, deferred);
+				me.success(response, token); // NEW
             },
 
             failure: function(response) {
                 //this.logger.debug("getPersonList.failure"); // logger is undefined here
 				console.log("getPersonList.failure");
-                me.failure(response, deferred);
+                //ORIGINAL me.failure(response, deferred);
+				me.failure(response, token); // NEW
             }
         });		
 		
-        return deferred.promise;
+        //ORIGINAL return deferred.promise;
+		return token; // NEW
     }
-	
-    /**
-     * The login ajax service call. Hits a json service and handles the success and fault accordingly.
-     *
-     * @param {String} username The username being authenticated.
-     * @param {String} password The password being authenticated.
-     */
-    // authenticate: function(username, password) {
-        // this.logger.debug("authenticate: username = " + username + ", password = " + password);
-
-        // var deferred = Ext.create("Deft.promise.Deferred");
-        // var me = this;
-
-        // Ext.Ajax.request({
-            // url: "data/login-success.json",
-// //            //url: "http://localhost:8080/SenchaDemo",
-            // method: "post",
-            // params: {
-                // j_username: username,
-                // j_password: password
-            // },
-
-            // success: function(response) {
-                // this.logger.debug("authenticate.success");
-                // me.success(response, deferred);
-            // },
-
-            // failure: function(response) {
-                // this.logger.debug("authenticate.failure");
-                // me.failure(response, deferred);
-            // }
-        // });
-
-        // return deferred.promise;
-    // },
-
-    /**
-     * The logout ajax service call. Hits a json service and handles the success and fault accordingly.
-     */
-    // logout: function() {
-        // this.logger.debug("logout");
-
-        // var me = this;
-
-        // Ext.Ajax.request({
-            // url: "data/logout-success.json",
-            // method: "post",
-
-            // success: function(response) {
-                // this.logger.debug("logout.success");
-
-                // var response = Ext.JSON.decode(response.responseText);
-                // me.success(response);
-            // },
-
-            // failure: function(response) {
-                // this.logger.debug("logout.failure");
-                // me.failure(response);
-            // }
-        // });
-    // }	
 	
 });    

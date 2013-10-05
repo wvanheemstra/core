a6859baf7fb5e04a428701c47dae03b41033e409
wvanheemstra <wvanheemstra@icloud.com>
2013-10-05 13:29:03 +0100

    Updated

---
+++ public/app/event/organisation/group/Event.js
@@ -0,0 +1,111 @@
+/**
+ * The organisation group event contains data and event types to perform CRUD operations on organisation groups.
+ */
+Ext.define("Core.event.organisation.group.Event", {
+    extend: "FlowMVC.mvc.event.AbstractEvent",
+    
+    statics: {	
+		
+        /**
+         * The get organisation group event type.
+         */
+        GET_ORGANISATION_GROUP:               "getOrganisationGroup",
+
+        /**
+         * The get organisation group success event type.
+         */
+        GET_ORGANISATION_GROUP_SUCCESS:       "getOrganisationGroupSuccess",
+
+        /**
+         * The get organisation group failure event type.
+         */
+        GET_ORGANISATION_GROUP_FAILURE:       "getOrganisationGroupFailure",
+
+        /**
+         * The create organisation group event type.
+         */
+        CREATE_ORGANISATION_GROUP:            "createOrganisationGroup",
+
+        /**
+         * The create organisation group success event type.
+         */
+        CREATE_ORGANISATION_GROUP_SUCCESS:    "createOrganisationGroupSuccess",
+
+        /**
+         * The create organisation group failure event type.
+         */
+        CREATE_ORGANISATION_GROUP_FAILURE:    "createOrganisationGroupFailure",
+
+        /**
+         * The update organisation group event type.
+         */
+        UPDATE_ORGANISATION_GROUP:            "updateOrganisationGroup",
+
+        /**
+         * The update organisation group success event type.
+         */
+        UPDATE_ORGANISATION_GROUP_SUCCESS:    "updateOrganisationGroupSuccess",
+
+        /**
+         * The update organisation group failure event type.
+         */
+        UPDATE_ORGANISATION_GROUP_FAILURE:    "updateOrganisationGroupFailure",
+
+        /**
+         * The delete organisation group event type.
+         */
+        DELETE_ORGANISATION_GROUP:            "deleteOrganisationGroup",
+
+        /**
+         * The delete organisation group success event type.
+         */
+        DELETE_ORGANISATION_GROUP_SUCCESS:    "deleteOrganisationGroupSuccess",
+
+        /**
+         * The delete organisation group failure event type.
+         */
+        DELETE_ORGANISATION_GROUP_FAILURE:    "deleteOrganisationGroupFailure",
+		
+		/**
+         * The read organisation groups event type.
+         */
+        READ_ORGANISATION_GROUPS:            "readOrganisationGroups",
+
+        /**
+         * The read organisation groups success event type.
+         */
+        READ_ORGANISATION_GROUPS_SUCCESS:    "readOrganisationGroupsSuccess",
+
+        /**
+         * The read organisation groups failure event type.
+         */
+        READ_ORGANISATION_GROUPS_FAILURE:    "readOrganisationGroupsFailure"
+		
+    },
+
+    /**
+     * @property {Number} id [readOnly=false]
+     * The id of a organisation group.
+     */
+    id: null,
+
+    /**
+     * @property {Core.model.organisation.group.Model} organisationGroup [readOnly=false]
+     * A organisation group to perform CRUD actions on.
+     */
+    organisationGroup: null,
+
+    /**
+     * Constructor. 
+     *
+     * @param {String} type The event type.
+     * @param {Number} id The id of the organisation group the CRUD operation is acting on.
+     * @param {Core.model.organisation.group.Model} organisationGroup The organisation group the CRUD operation is acting on.     
+     */
+    constructor: function(type, id, organisationGroup) {
+        this.callParent(arguments);
+
+        this.id = id;
+        this.organisationGroup = organisationGroup;
+    }
+})	
---
+++ public/app/event/person/group/Event.js
@@ -0,0 +1,111 @@
+/**
+ * The person group event contains data and event types to perform CRUD operations on person groups.
+ */
+Ext.define("Core.event.person.group.Event", {
+    extend: "FlowMVC.mvc.event.AbstractEvent",
+    
+    statics: {	
+		
+        /**
+         * The get person group event type.
+         */
+        GET_PERSON_GROUP:               "getPersonGroup",
+
+        /**
+         * The get person group success event type.
+         */
+        GET_PERSON_GROUP_SUCCESS:       "getPersonGroupSuccess",
+
+        /**
+         * The get person group failure event type.
+         */
+        GET_PERSON_GROUP_FAILURE:       "getPersonGroupFailure",
+
+        /**
+         * The create person group event type.
+         */
+        CREATE_PERSON_GROUP:            "createPersonGroup",
+
+        /**
+         * The create person group success event type.
+         */
+        CREATE_PERSON_GROUP_SUCCESS:    "createPersonGroupSuccess",
+
+        /**
+         * The create person group failure event type.
+         */
+        CREATE_PERSON_GROUP_FAILURE:    "createPersonGroupFailure",
+
+        /**
+         * The update person group event type.
+         */
+        UPDATE_PERSON_GROUP:            "updatePersonGroup",
+
+        /**
+         * The update person group success event type.
+         */
+        UPDATE_PERSON_GROUP_SUCCESS:    "updatePersonGroupSuccess",
+
+        /**
+         * The update person group failure event type.
+         */
+        UPDATE_PERSON_GROUP_FAILURE:    "updatePersonGroupFailure",
+
+        /**
+         * The delete person group event type.
+         */
+        DELETE_PERSON_GROUP:            "deletePersonGroup",
+
+        /**
+         * The delete person group success event type.
+         */
+        DELETE_PERSON_GROUP_SUCCESS:    "deletePersonGroupSuccess",
+
+        /**
+         * The delete person group failure event type.
+         */
+        DELETE_PERSON_GROUP_FAILURE:    "deletePersonGroupFailure",
+		
+		/**
+         * The read person groups event type.
+         */
+        READ_PERSON_GROUPS:            "readPersonGroups",
+
+        /**
+         * The read person groups success event type.
+         */
+        READ_PERSON_GROUPS_SUCCESS:    "readPersonGroupsSuccess",
+
+        /**
+         * The read person groups failure event type.
+         */
+        READ_PERSON_GROUPS_FAILURE:    "readPersonGroupsFailure"
+		
+    },
+
+    /**
+     * @property {Number} id [readOnly=false]
+     * The id of a person group.
+     */
+    id: null,
+
+    /**
+     * @property {Core.model.person.group.Model} personGroup [readOnly=false]
+     * A person group to perform CRUD actions on.
+     */
+    personGroup: null,
+
+    /**
+     * Constructor. 
+     *
+     * @param {String} type The event type.
+     * @param {Number} id The id of the person group the CRUD operation is acting on.
+     * @param {Core.model.person.group.Model} personGroup The person group the CRUD operation is acting on.     
+     */
+    constructor: function(type, id, personGroup) {
+        this.callParent(arguments);
+
+        this.id = id;
+        this.personGroup = personGroup;
+    }
+})	

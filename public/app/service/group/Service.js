0611ca064c476af0f13077b74c9992fc10bcf733
wvanheemstra <wvanheemstra@icloud.com>
2013-10-06 17:13:12 +0100

    Updated

--- public/app/service/group/Service.js
+++ public/app/service/group/Service.js
@@ -5,7 +5,8 @@ Ext.define("Core.service.group.Service", {
     extend: "FlowMVC.mvc.service.AbstractService",
 
     inject: [
-        "logger"
+        "logger",
+		"groupStore"
     ],
 	
     /**
@@ -17,6 +18,77 @@ Ext.define("Core.service.group.Service", {
         //ORIGINAL var deferred = Ext.create("Deft.promise.Deferred");
 		var token = this.getTokenOrPromise(); // NEW
         var me = this;
+		
+		// Set the JSON data for the request
+		var jsonData = {
+			"pagination" :  {
+				"page": 0,
+				"limit": 50,
+				"sort": "GroupName",
+				"dir": "ASC"
+			},
+			"basicInfo" : {
+				"ccoId": "Core",
+				"prefLang": "eng_GB",
+				"requestStartDate": (new Date()).toISOString(),
+				"requesterApp": "Group"
+			},
+			"mql" : {
+				"query" : [{
+					"type": "/core/group",
+					"kp_GroupID": null,
+					"GroupName": null
+				}]
+			},
+			"debug_info" : {
+				
+			}
+		};
+		
+		// Update the store for the JSON data
+		var proxy = me.groupStore.getProxy();
+		proxy['jsonData'] = jsonData;
+		var operation = Ext.create('Ext.data.Operation');
+			operation.setAction('read');
+			operation.setUrl(); // TO DO
+		//	operation.setPassword(); // TO DO
+		//	operation.setUsername(); // TO DO
+		var callback = null;
+		var scope = me;
+		var request = me.groupStore.getProxy().doRequest(operation, callback, scope);
+		
+		// Now load the store
+		me.groupStore.load({
+			params: {}, //WAS {'id':d.data.category_id},
+			scope: this,
+			callback: function(records, operation, success) {
+				if (success) {
+					//WAS console.log("Category: " + category.get('name'));
+					//this.logger.debug("readGroups.success"); // logger is undefined here
+					console.log("readGroups.success");
+					if(typeof response.responseText !== 'undefined'){
+						// Covert the string Array to an Object
+						response = JSON.parse(response.responseText);
+					}
+					//ORIGINAL me.success(response, deferred);
+					me.success(response, token); // NEW
+				} else {
+					//WAS console.log('error');
+					//this.logger.debug("readGroups.failure"); // logger is undefined here
+					console.log("readGroups.failure");
+					if(typeof response.responseText !== 'undefined'){
+						// Covert the string Array to an Object
+						response = JSON.parse(response.responseText);
+					}				
+					//ORIGINAL me.failure(response, deferred);
+					me.failure(response, token); // NEW
+				}
+			}
+		});
+		
+		// No more need for an Ajax request here....
+		// But how do we catch success or failure??
+		/*
 		
         Ext.Ajax.request({
             url: "data/read-groups-success.json",
@@ -49,6 +121,7 @@ Ext.define("Core.service.group.Service", {
 				me.failure(response, token); // NEW
             }
         });		
+		*/
 		
         //ORIGINAL return deferred.promise;
 		return token; // NEW

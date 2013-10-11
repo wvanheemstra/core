eb3a2521b4736327b41b2e3e19daf34d852a4268
wvanheemstra <wvanheemstra@icloud.com>
2013-10-11 14:15:03 +0100

    Updated

--- public/app/service/organisation/mock/Service.js
+++ public/app/service/organisation/mock/Service.js
@@ -7,129 +7,6 @@ Ext.define("Core.service.organisation.mock.Service", {
     inject: [
         "logger"
     ],
-
-    /**
-     * The mock service call.
-     */
-    getOrganisationSlide: function() {
-        this.logger.debug("getOrganisationSlide");
-
-        var response = {
-            success: true,
-            organisationSlide: [
-                { id: 0,    OrganisationName: "Atlas Ltd." },
-                { id: 1,    OrganisationName: "Brachium Ltd." },
-                { id: 2,    OrganisationName: "Cursa & Co." },
-                { id: 3,    OrganisationName: "Diadem Corp." },
-                { id: 4,    OrganisationName: "Electra & Sons" },
-                { id: 5,    OrganisationName: "Fornacis Holding" },
-                { id: 6,    OrganisationName: "Grafias Inc." },
-                { id: 7,    OrganisationName: "Homam Enterprise" },
-                { id: 8,    OrganisationName: "Izar Services" },
-                { id: 9,    OrganisationName: "Jabbah Ltd." },
-                { id: 10,   OrganisationName: "Kuma Corp." },
-                { id: 11,   OrganisationName: "Lesath & Co." },
-                { id: 12,   OrganisationName: "Matar Ind." },
-                { id: 13,   OrganisationName: "Naos & Co." },
-                { id: 14,   OrganisationName: "Oxyn Ltd." },
-                { id: 15,   OrganisationName: "Propus Holding" },
-                { id: 16,   OrganisationName: "Quebec Limited" },    
-                { id: 17,   OrganisationName: "Rana Productions" },
-                { id: 18,   OrganisationName: "Salm Inc." },
-                { id: 19,   OrganisationName: "Thabit Ltd." },
-                { id: 20,   OrganisationName: "Universe Unlimited" },   
-                { id: 21,   OrganisationName: "Vega Stores" },
-                { id: 22,   OrganisationName: "Wasat Inc." },
-                { id: 23,   OrganisationName: "Xanadu & Sons" },
-                { id: 24,   OrganisationName: "Yildun Industrial" },
-                { id: 25,   OrganisationName: "Zibla & Co." }
-            ]
-        };
-
-        return this.delayedSuccess(response);
-    },
-    
-    /**
-     * The mock service call.
-     */
-    getOrganisationList: function() {
-        this.logger.debug("getOrganisationList");
-
-        var response = {
-            success: true,
-            organisationList: [
-                { id: 0,    OrganisationName: "Atlas Ltd." },
-                { id: 1,    OrganisationName: "Brachium Ltd." },
-                { id: 2,    OrganisationName: "Cursa & Co." },
-                { id: 3,    OrganisationName: "Diadem Corp." },
-                { id: 4,    OrganisationName: "Electra & Sons" },
-                { id: 5,    OrganisationName: "Fornacis Holding" },
-                { id: 6,    OrganisationName: "Grafias Inc." },
-                { id: 7,    OrganisationName: "Homam Enterprise" },
-                { id: 8,    OrganisationName: "Izar Services" },
-                { id: 9,    OrganisationName: "Jabbah Ltd." },
-                { id: 10,   OrganisationName: "Kuma Corp." },
-                { id: 11,   OrganisationName: "Lesath & Co." },
-                { id: 12,   OrganisationName: "Matar Ind." },
-                { id: 13,   OrganisationName: "Naos & Co." },
-                { id: 14,   OrganisationName: "Oxyn Ltd." },
-                { id: 15,   OrganisationName: "Propus Holding" },
-                { id: 16,   OrganisationName: "Quebec Limited" },    
-                { id: 17,   OrganisationName: "Rana Productions" },
-                { id: 18,   OrganisationName: "Salm Inc." },
-                { id: 19,   OrganisationName: "Thabit Ltd." },
-                { id: 20,   OrganisationName: "Universe Unlimited" },   
-                { id: 21,   OrganisationName: "Vega Stores" },
-                { id: 22,   OrganisationName: "Wasat Inc." },
-                { id: 23,   OrganisationName: "Xanadu & Sons" },
-                { id: 24,   OrganisationName: "Yildun Industrial" },
-                { id: 25,   OrganisationName: "Zibla & Co." }
-            ]
-        };
-
-        return this.delayedSuccess(response);
-    },
-    
-    /**
-     * The mock service call.
-     */
-    getOrganisationTile: function() {
-        this.logger.debug("getOrganisationTile");
-
-        var response = {
-            success: true,
-            organisationTile: [
-                { id: 0,    OrganisationName: "Atlas Ltd." },
-                { id: 1,    OrganisationName: "Brachium Ltd." },
-                { id: 2,    OrganisationName: "Cursa & Co." },
-                { id: 3,    OrganisationName: "Diadem Corp." },
-                { id: 4,    OrganisationName: "Electra & Sons" },
-                { id: 5,    OrganisationName: "Fornacis Holding" },
-                { id: 6,    OrganisationName: "Grafias Inc." },
-                { id: 7,    OrganisationName: "Homam Enterprise" },
-                { id: 8,    OrganisationName: "Izar Services" },
-                { id: 9,    OrganisationName: "Jabbah Ltd." },
-                { id: 10,   OrganisationName: "Kuma Corp." },
-                { id: 11,   OrganisationName: "Lesath & Co." },
-                { id: 12,   OrganisationName: "Matar Ind." },
-                { id: 13,   OrganisationName: "Naos & Co." },
-                { id: 14,   OrganisationName: "Oxyn Ltd." },
-                { id: 15,   OrganisationName: "Propus Holding" },
-                { id: 16,   OrganisationName: "Quebec Limited" },    
-                { id: 17,   OrganisationName: "Rana Productions" },
-                { id: 18,   OrganisationName: "Salm Inc." },
-                { id: 19,   OrganisationName: "Thabit Ltd." },
-                { id: 20,   OrganisationName: "Universe Unlimited" },   
-                { id: 21,   OrganisationName: "Vega Stores" },
-                { id: 22,   OrganisationName: "Wasat Inc." },
-                { id: 23,   OrganisationName: "Xanadu & Sons" },
-                { id: 24,   OrganisationName: "Yildun Industrial" },
-                { id: 25,   OrganisationName: "Zibla & Co." }
-            ]
-        };
-
-        return this.delayedSuccess(response);
-    },
     
     /**
      * The mock service call.
@@ -140,8 +17,8 @@ Ext.define("Core.service.organisation.mock.Service", {
         var response = {
             success: true,
             organisation: {
-                id: this.getRandomInt(1000, 99999),
-                OrganisationName: organisation.OrganisationName
+                kp_OrganisationID: this.getRandomInt(1000, 99999),
+                OrganisationName: organisation.OrganisationName			
             }
         };
 
@@ -153,13 +30,13 @@ Ext.define("Core.service.organisation.mock.Service", {
      * The mock service call.
      */
     updateOrganisation: function(organisation) {
-        this.logger.debug("updateOrganisation: id = ", organisation.id);
+        this.logger.debug("updateOrganisation: kp_OrganisationID = " + organisation.kp_OrganisationID);
 
         var response = {
             success: true,
             organisation: {
-                id: organisation.id,
-                OrganisationName: organisation.OrganisationName
+                kp_OrganisationID: organisation.kp_OrganisationID,
+                OrganisationName: organisation.OrganisationName		
             }
         };
 
@@ -171,17 +48,59 @@ Ext.define("Core.service.organisation.mock.Service", {
      * The mock service call.
      */
     deleteOrganisation: function(organisation) {
-        this.logger.debug("deleteOrganisation: id = ", organisation.id);
+        this.logger.debug("deleteOrganisation: kp_OrganisationID = " + organisation.kp_OrganisationID);
 
         var response = {
             success: true,
             organisation: {
-                id: organisation.id,
+                kp_OrganisationID: organisation.kp_OrganisationID,
                 OrganisationName: organisation.OrganisationName
             }
         };
 
         response = Ext.create("Core.model.organisation.Model", response.organisation);
         return this.delayedSuccess(response);
-    }      
+    }, 
+
+    /**
+     * The mock service call.
+     */
+    readOrganisations: function() {
+        this.logger.debug("readOrganisations");
+
+        var response = {
+            success: true,
+            organisations: [
+                { kp_OrganisationID: 1,    OrganisationName: "Adam" },
+                { kp_OrganisationID: 2,    OrganisationName: "Bob" },
+                { kp_OrganisationID: 3,    OrganisationName: "Carol" },
+                { kp_OrganisationID: 4,    OrganisationName: "David" },
+                { kp_OrganisationID: 5,    OrganisationName: "Eddie" },
+                { kp_OrganisationID: 6,    OrganisationName: "Frank" },
+                { kp_OrganisationID: 7,    OrganisationName: "George" },
+                { kp_OrganisationID: 8,    OrganisationName: "Harry" },
+                { kp_OrganisationID: 9,    OrganisationName: "Ike" },
+                { kp_OrganisationID: 10,   OrganisationName: "Jim" },
+                { kp_OrganisationID: 11,   OrganisationName: "Kenny" },
+                { kp_OrganisationID: 12,   OrganisationName: "Larry" },
+                { kp_OrganisationID: 13,   OrganisationName: "Mary" },
+                { kp_OrganisationID: 14,   OrganisationName: "Nancy" },
+                { kp_OrganisationID: 15,   OrganisationName: "Olivier" },
+                { kp_OrganisationID: 16,   OrganisationName: "Peter" },
+                { kp_OrganisationID: 17,   OrganisationName: "Quincy" },
+                { kp_OrganisationID: 18,   OrganisationName: "Roger" },
+                { kp_OrganisationID: 19,   OrganisationName: "Sam" },
+                { kp_OrganisationID: 20,   OrganisationName: "Thomas" },
+                { kp_OrganisationID: 21,   OrganisationName: "Uncle" },
+                { kp_OrganisationID: 22,   OrganisationName: "Vincent" },
+                { kp_OrganisationID: 23,   OrganisationName: "William" },
+                { kp_OrganisationID: 24,   OrganisationName: "Xavier" },
+                { kp_OrganisationID: 25,   OrganisationName: "Yogi" },
+                { kp_OrganisationID: 26,   OrganisationName: "Zachary" }
+            ]
+        };
+
+        return this.delayedSuccess(response);
+    }
+	
 });

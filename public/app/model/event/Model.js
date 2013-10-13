f545f17285d5d6b385abe82539788ad727217318
wvanheemstra <wvanheemstra@icloud.com>
2013-10-13 16:00:43 +0100

    Updated

--- public/app/model/event/Model.js
+++ public/app/model/event/Model.js
@@ -52,17 +52,6 @@ Ext.define("Core.model.event.Model", {
 	    ],
 		hasMany:[
 			{
-			  name: "Membership",
-			  associationKey: "Membership",
-			  instanceName: "Membership",
-			  getterName: "getMembership",
-			  setterName: "setMembership",			  
-			  model: "Core.model.membership.Model",
-			  primaryKey: "kp_EventID", // the field in the parent that identifies it.
-			  foreignKey: "kf_EventID", // the key that identifies the parent in the child. In a belongsTo or hasOne relation, this is part of the model itself, in a hasMany relation this is a field of the child objects that refer to my Id.
-			  foreignStore: "membershipStore",  //WAS "Core.store.date.Store", // the store name that contains the related records
-			  foreignStoreId: "membershipStore" // the store id  of the foreign store
-			},{
 			  name: "EventGroup",
 			  associationKey: "EventGroup",
 			  instanceName: "EventGroup",
@@ -165,17 +154,6 @@ Ext.define("Core.model.event.Model", {
 	],
 	hasMany:[
 		{
-		  name: "Membership",
-		  associationKey: "Membership",	
-		  instanceName: "Membership",
-		  getterName: "getMembership",
-		  setterName: "setMembership",			  
-		  model: "Core.model.membership.Model",
-		  primaryKey: "kp_EventID", // the field in the parent that identifies it.
-		  foreignKey: "kf_EventID", // the key that identifies the parent in the child. In a belongsTo or hasOne relation, this is part of the model itself, in a hasMany relation this is a field of the child objects that refer to my Id.
-		  foreignStore: "membershipStore",  //WAS "Core.store.date.Store", // the store name that contains the related records
-		  foreignStoreId: "membershipStore" // the store id  of the foreign store
-		},{
 		  name: "EventGroup",
 		  associationKey: "EventGroup",	
 		  instanceName: "EventGroup",

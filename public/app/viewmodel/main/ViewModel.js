63c4342d7e1445a46946fcbeb31069d0647f584b
wvanheemstra <wvanheemstra@icloud.com>
2013-10-24 22:31:04 +0100

    Updated

--- public/app/viewmodel/main/ViewModel.js
+++ public/app/viewmodel/main/ViewModel.js
@@ -1,11 +1,22 @@
 glu.defModel('Core.assets.main', {
 	alias:'widget.mainViewModel',
-    options:{
-        mtype:'options'
-    },
-
+	
+	/*
+	 * Properties - Hold states that various parts of the screen can be in. 
+	 * Usually correspond to things that the user can set (like the contents of a text field, 
+	 * or the currently active tab, or which rows of a grid are selected).
+	 */
     sliceCounter:1,
 
+	/*
+	 * Sub-models - Contains various sub-screens and lists of sub-screens 
+	 * (glu is for full applications so view models are always in a hierarchy with a single root). 
+	 * There is also a special 'parentVM' property to find any view model's container.
+	 */
+    options:{
+        mtype:'options'
+    },	 
+	 
     assetSetList:{
         mtype:'activatorlist',
         focusProperty:'assetSetWithFocus'
@@ -16,9 +27,16 @@ glu.defModel('Core.assets.main', {
 		name:'dummy'
 	},
 
+	/*
+	 * Formulas - Calculated properties that respond to changes in properties or other formulas. 
+	 * By their nature, they are read-only so they typically represent the app 'responding' to user interaction. 
+	 * Glu will analyze the formula and keep it updated when input properties change.
+	 * To declare a formula, put a $ at the end of the name (this won't become part of its name but is just a flag) 
+	 * and then supply a function that returns a value.
+	 */
     title$: function(){
 		console.log("title$");
-        return this.localize('title',{focusName: this.assetSetWithFocus.name}); // TEMP commented out by wvh
+        return this.localize('title',{focusName: this.assetSetWithFocus.name});
     },
 
     detail:{ 
@@ -30,7 +48,10 @@ glu.defModel('Core.assets.main', {
         this.cloneSet(); //clone dummy asset set
     },
 
-    //COMMANDS
+    /*
+	 * Commands - Actions that the user can take that aren't represented by simple properties. 
+	 * For instance, a save button or hitting the 'close window' indicator.
+	 */
     cloneSet:function () {
 		console.log("cloneSet");	
         var newSlice = this.model(this.assetSetWithFocus.clone());
@@ -45,7 +66,13 @@ glu.defModel('Core.assets.main', {
         this.open(this.options);
     },
 
-    //REACTIONS
+    /*
+	 * Reactors - Rules that are triggered on property / formula changes 
+	 * so you don't have to put all of your side-effects into the property setter. 
+	 * For instance, refreshing the grid when any of several filters change. 
+	 * A formula is really a special type of reactor where the action is setting a single property; 
+	 * if it's more complicated, use a reactor.
+	 */
     when_selected_asset_changes_then_load_detail:{
         on:['assetSetWithFocusChanged','assetSetWithFocus.assetWithFocusChanged'],
         action:function () {
@@ -55,7 +82,9 @@ glu.defModel('Core.assets.main', {
         }
     },
 
-    //EXTERNAL
+    /*
+	 * Externals
+	 */
     notifyAssetChanged:function () {
 		console.log("notifyAssetChanged");		
         this.assetSetList.getActiveItem().refreshAssetList();

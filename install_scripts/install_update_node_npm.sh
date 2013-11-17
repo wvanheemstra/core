35c19d42ecfe47d3972583ef3ffd98972a43a934
wvanheemstra <wvanheemstra@icloud.com>
2013-11-17 13:52:50 +0000

    Updated

---
+++ install_scripts/install_update_node_npm.sh
@@ -0,0 +1,20 @@
+# install and/or update nodejs and npm from git
+#
+# this way is best if you want to stay up to date
+# or submit patches to node or npm
+ 
+mkdir ~/local
+echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
+. ~/.bashrc
+ 
+# could also fork, and then clone your own fork instead of the official one
+ 
+git clone git://github.com/joyent/node.git
+cd node
+./configure --prefix=~/local
+make install
+cd ..
+ 
+git clone git://github.com/isaacs/npm.git
+cd npm
+make install # or `make link` for bleeding edge

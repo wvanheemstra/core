df893e4cb6903e45972c62c1ccdce506e3d0babe
wvanheemstra <wvanheemstra@icloud.com>
2013-12-06 12:29:13 +0000

    Updated

--- public/app/store/booking/Store.js
+++ public/app/store/booking/Store.js
@@ -1,8 +1,8 @@
 /**
  * Contains the list of booking objects.
  */
-Ext.define("Core.store.booking.Store", {
-    extend: "FlowMVC.mvc.store.AbstractStore",
+Ext.define("Core.store.booking.Store", Sencha.storeCompatibility({
+    extend: "Core.store.booking.base.Store",
     // Touch uses properties inside of config
 	config: {
 		storeId: "bookingStore", // Required for automatic registration	
@@ -18,19 +18,6 @@ Ext.define("Core.store.booking.Store", {
 	            }
 	        }
 	    }
-	},//eof config
-	// Ext requires properties outside of config
-	storeId: "bookingStore", // Required for automatic registration		
-    model: "Core.model.booking.Model",
-    sorters: "BookingName",
-    isAutoUpdate: true,
-    grouper: {
-        groupFn: function(record) {
-            try {
-                return record.get("BookingName")[0].toUpperCase(); // so 'van Halen' comes under 'V'
-            } catch(err) {
-
-            }
-        }
-    }	
-});
+	}//eof config
+}) //eof storeCompatibility	
+);

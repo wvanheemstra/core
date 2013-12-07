87954ca1793c1c6e1ada706894650ddf3555f10d
wvanheemstra <wvanheemstra@icloud.com>
2013-12-07 10:36:24 +0000

    Updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,40 @@
+/*global module, global*/
 module.exports = function (grunt) {
-   "use strict";
+	"use strict";
+	grunt.initConfig({
+        /**
+         * Clean
+         *
+         * Before generating any new files, remove any previously-created output files.
+         */
+        clean: {
+            build: ["build"]
+        },
+
+        /**
+         * JSHint
+         *
+         * Validate the source code files to ensure they follow our coding convention and
+         * don"t contain any common errors.
+         */
+        jshint: {
+            all: [
+                "Gruntfile.js",
+                "public/**/*.js",
+                "!public/resources/**/*.js"
+            ],
+            options: {
+                jshintrc: ".jshintrc"
+            }
+        }   
+   
+    });//eof initConfig 
+
+	grunt.loadNpmTasks("grunt-contrib-jshint");	
+    grunt.loadNpmTasks("grunt-contrib-clean");
+
+    grunt.registerTask("default", [
+        "jshint", "clean:build"
+    ]);	
+	
 };

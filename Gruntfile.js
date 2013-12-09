46a51e1a104a8242f6b7ddcd96baefa833e82f62
wvanheemstra <wvanheemstra@icloud.com>
2013-12-09 18:19:59 +0000

    Updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,65 +2,80 @@
 module.exports = function (grunt) {
 	"use strict";
 	
-	var dateFormat = require('dateformat');
-	
-	grunt.initConfig({
-        /**
-         * Clean
-         *
-         * Before generating any new files, remove any previously-created output files.
-         */
-        clean: {
-            build: ["build"]
-        },
+	var dateFormat = require("dateformat");
+	var tests = "test/**/*.js";
+	var tasks = "task/**/*.js";
+	var reportDir = "build/reports/" + dateFormat(new Date(), "yyyymmdd-HHMMss");
 
-        /**
-         * JSHint
-         *
-         * Validate the source code files to ensure they follow our coding convention and
-         * don"t contain any common errors.
-         */
-        jshint: {
-            all: [
-                "Gruntfile.js",
-                "public/**/*.js",
-                "!public/resources/**/*.js"
-            ],
-            options: {
-                jshintrc: ".jshintrc"
-            }
-        },	
+	grunt.initConfig({
+		/**
+		 * Clean
+		 *
+		 * Before generating any new files, remove any previously-created output files.
+		 */
+		clean: {
+			build: ["build"]
+		},
 		
-        /**
-         * Sencha Dependencies
+		/**
+		 * Watch
+		 *
+		 */
+		watch: {
+			files: [ tasks, tests ],
+			tasks: "default"
+		},
+
+		/**
+		 * JSHint
+		 *
+		 * Validate the source code files to ensure they follow our coding convention and
+		 * don"t contain any common errors.
+		 */
+		jshint: {
+			all: [
+				"Gruntfile.js",
+	//			"public/**/*.js", // TEMP commented out
+				tasks,
+				tests,
+				"!public/resources/**/*.js"
+			],
+			options: {
+				jshintrc: ".jshintrc"
+			},
+			globals: {}
+		},
+		
+		/**
+		 * Sencha Dependencies
 		 *
 		 * Get ordered project Sencha dependencies
 		 * See https://github.com/mattgoldspink/grunt-sencha-dependencies
 		 * 
-         */			
+		*/
 		sencha_dependencies: {
 			asset_desktop: {
 				options : {
-					appFile: 'require/asset/desktop.js',
-					pageToProcess: 'asset-desktop.html',
-					pageRoot: 'public',
-					senchaDir: 'resources/js/ext'
+					appFile: "require/asset/desktop.js",
+					pageToProcess: "asset-desktop.html",
+					pageRoot: "public",
+					senchaDir: "resources/js/ext"
 				}
 			},
 			asset_phone: {
 				options : {
-					appFile: 'require/asset/phone.js',
-					pageToProcess: 'asset-phone.html',
-					pageRoot: 'public',
-					senchaDir: 'resources/js/touch'
+					appFile: "require/asset/phone.js",
+					pageToProcess: "asset-phone.html",
+					pageRoot: "public",
+					senchaDir: "resources/js/touch"
 				}
 			},
 			asset_tablet: {
 				options : {
-					appFile: 'require/asset/tablet.js',
-					pageToProcess: 'asset-tablet.html',
-					pageRoot: 'public',
-					senchaDir: 'resources/js/touch'
+					appFile: "require/asset/tablet.js",
+					pageToProcess: "asset-tablet.html",
+					pageRoot: "public",
+					senchaDir: "resources/js/touch"
 				}
 			}
         },	
@@ -72,22 +87,22 @@ module.exports = function (grunt) {
          */		
 		jasmine: {
 			asset_desktop: {
-			  src: '<%= sencha_dependencies_asset_desktop_app %>',
-			  options: {
-				specs: 'test/specs/app/view/extjs/viewport/asset/View.js'
-			  }
+				src: "<%= sencha_dependencies_asset_desktop_app %>",
+				options: {
+					specs: "test/specs/app/view/extjs/viewport/asset/View.js"
+				}
 			},
 			asset_phone: {
-			  src: '<%= sencha_dependencies_asset_phone_app %>',
-			  options: {
-				specs: 'test/specs/app/mediator/touch/viewport/asset/Mediator.js'
-			  }
+				src: "<%= sencha_dependencies_asset_phone_app %>",
+				options: {
+					specs: "test/specs/app/mediator/touch/viewport/asset/Mediator.js"
+				}
 			},
 			asset_tablet: {
-			  src: '<%= sencha_dependencies_asset_tablet_app %>',
-			  options: {
-				specs: 'test/specs/app/mediator/touch/viewport/asset/Mediator.js'
-			  }
+				src: "<%= sencha_dependencies_asset_tablet_app %>",
+				options: {
+					specs: "test/specs/app/mediator/touch/viewport/asset/Mediator.js"
+				}
 			}			
 		},		
 		
@@ -98,65 +113,65 @@ module.exports = function (grunt) {
 		 */
 		jsduck: {
 			asset_desktop: {
-			  src: [
-				'public/app/config/global/Config.js',
-				'public/app/config/asset/**/*.js',
-				'public/app/view/extjs/login/**/*.js',
-				'public/app/view/extjs/asset/**/*.js',
-				'public/app/view/extjs/viewport/asset/**/*.js'
-			  ],
-			  dest: 'public/app/doc/asset/desktop',
-			  options: {
-				'title': 'Core - Asset [desktop]'
-			  }
+				src: [
+					"public/app/config/global/Config.js",
+					"public/app/config/asset/**/*.js",
+					"public/app/view/extjs/login/**/*.js",
+					"public/app/view/extjs/asset/**/*.js",
+					"public/app/view/extjs/viewport/asset/**/*.js"
+				],
+				dest: "public/app/doc/asset/desktop",
+				options: {
+					"title": "Core - Asset [desktop]"
+				}
 			},
 			asset_phone: {
-			  src: [
-				'public/app/config/global/Config.js',
-				'public/app/config/asset/**/*.js',
-				'public/app/view/touch/login/**/*.js',
-				'public/app/view/touch/asset/**/*.js',
-				'public/app/mediator/touch/asset/**/*.js',			
-				'public/app/mediator/touch/viewport/asset/**/*.js'
-			  ],
-			  dest: 'public/app/doc/asset/phone',
-			  options: {
-				'title': 'Core - Asset [phone]'
-			  }
+				src: [
+					"public/app/config/global/Config.js",
+					"public/app/config/asset/**/*.js",
+					"public/app/view/touch/login/**/*.js",
+					"public/app/view/touch/asset/**/*.js",
+					"public/app/mediator/touch/asset/**/*.js",			
+					"public/app/mediator/touch/viewport/asset/**/*.js"
+				],
+				dest: "public/app/doc/asset/phone",
+				options: {
+					"title": "Core - Asset [phone]"
+				}
 			},
 			asset_tablet: {
-			  src: [
-				'public/app/config/global/Config.js',
-				'public/app/config/asset/**/*.js',
-				'public/app/view/touch/login/**/*.js',
-				'public/app/view/touch/asset/**/*.js',
-				'public/app/mediator/touch/asset/**/*.js',			
-				'public/app/mediator/touch/viewport/asset/**/*.js'
-			  ],
-			  dest: 'public/app/doc/asset/tablet',
-			  options: {
-				'title': 'Core - Asset [tablet]'
-			  }
-			}			
+				src: [
+					"public/app/config/global/Config.js",
+					"public/app/config/asset/**/*.js",
+					"public/app/view/touch/login/**/*.js",
+					"public/app/view/touch/asset/**/*.js",
+					"public/app/mediator/touch/asset/**/*.js",
+					"public/app/mediator/touch/viewport/asset/**/*.js"
+				],
+				dest: "public/app/doc/asset/tablet",
+				options: {
+					"title": "Core - Asset [tablet]"
+				}
+			}
 		}
-   
-    });//eof initConfig 
 
-	grunt.loadNpmTasks("grunt-contrib-jshint");	
-    grunt.loadNpmTasks("grunt-contrib-clean");
-	grunt.loadNpmTasks("grunt-sencha-dependencies");	
+	});//eof initConfig 
+
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-clean");
+	grunt.loadNpmTasks("grunt-sencha-dependencies");
 	grunt.loadNpmTasks("grunt-contrib-jasmine");
 	grunt.loadNpmTasks("grunt-jsduck");
 	grunt.loadNpmTasks("grunt-istanbul");
-	
-    grunt.registerTask("default", ["jshint", "clean:build"]);
+
+	grunt.registerTask("default", ["jshint", "clean:build"]);
 	grunt.registerTask("test_asset_desktop", ["sencha_dependencies:asset_desktop", "jasmine:asset_desktop"]);
 	grunt.registerTask("test_asset_phone", ["sencha_dependencies:asset_phone", "jasmine:asset_phone"]);
 	grunt.registerTask("test_asset_tablet", ["sencha_dependencies:asset_tablet", "jasmine:asset_tablet"]);
 	grunt.registerTask("test_all", ["sencha_dependencies:asset_desktop", "jasmine:asset_desktop"]);
 	grunt.registerTask("doc_asset_desktop", ["jsduck:asset_desktop"]);
 	grunt.registerTask("doc_asset_phone", ["jsduck:asset_phone"]);
-	grunt.registerTask("doc_asset_tablet", ["jsduck:asset_tablet"]);	
-	grunt.registerTask("doc_asset_all", ["jsduck:asset_desktop", "jsduck:asset_phone", "jsduck:asset_tablet"]);	
+	grunt.registerTask("doc_asset_tablet", ["jsduck:asset_tablet"]);
+	grunt.registerTask("doc_asset_all", ["jsduck:asset_desktop", "jsduck:asset_phone", "jsduck:asset_tablet"]);
 	
 };

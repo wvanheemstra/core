4d81612a727603f718b6d7831288936cab7a53ce
wvanheemstra <wvanheemstra@icloud.com>
2014-01-09 00:50:15 +0000

    Updated

---
+++ public/app/view/extjs/pubsub/View.js
@@ -0,0 +1,16 @@
+glu.defView('Core.assets.pubsub', {
+    alias:'widget.pubsubView',
+    title:'~~title~~',
+    xtype: 'form',
+    defaultType :'autofield',
+	width: 600,
+	height: 400,
+    defaults : {
+        labelWidth : 200
+    },
+    items:[
+//        'warnings',
+//        'offMaintenanceWarning',
+//        'missingWarning'
+    ]
+});
---
+++ public/app/viewmodel/pubsub/ViewModel.js
@@ -0,0 +1,22 @@
+glu.defModel('Core.assets.pubsub', {
+	alias:'widget.pubsubViewModel'//,
+//    warnings : true,
+//    offMaintenanceWarning : false,
+//    missingWarning : false,
+
+	/*
+	 * Formulas - Calculated properties that respond to changes in properties or other formulas. 
+	 * By their nature, they are read-only so they typically represent the app 'responding' to user interaction. 
+	 * Glu will analyze the formula and keep it updated when input properties change.
+	 * To declare a formula, put a $ at the end of the name (this won't become part of its name but is just a flag) 
+	 * and then supply a function that returns a value.
+	 */	
+//    offMaintenanceWarningIsEnabled$ : function(){
+//		console.log("offMaintenanceWarningIsEnabled$");	
+//        return this.warnings;
+//    },
+//    missingWarningIsEnabled$ : function(){
+//		console.log("missingWarningIsEnabled$");		
+//        return this.warnings;
+//    }
+});
